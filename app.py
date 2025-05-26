116c2c9326f68f3425e9b889298298572777b7ad
harish-fixplianceai <harish.m@compliancefoundry.ai>
2025-05-26 22:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Docker!"
+    return "Hello from jyothiram Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
